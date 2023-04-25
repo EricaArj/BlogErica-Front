f6a59b96259900704ca90cfe6555e1d92639245f
unknown <erika.f55@hotmail.com>
2023-04-24 22:44:18 -0300

    Ts

--- src/components/temas/listaTemas/ListaTemas.tsx
+++ src/components/temas/listaTemas/ListaTemas.tsx
@@ -1,67 +1,75 @@
-import {
-  Box,
-  Button,
-  Card,
-  CardActions,
-  CardContent,
-  Typography,
-} from "@mui/material";
-import React, { useEffect, useState } from "react";
-import { Tema } from '../../../models/Tema'
-import { getAll } from "../../../services/service";
-import useLocalStorage from "react-use-localstorage";
-import { useNavigate } from "react-router-dom";
+import React, { useEffect, useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import {Card, CardActions, CardContent, Button, Typography } from '@material-ui/core';
+import {Box} from '@mui/material';
+import './ListaTema.css';
+import Tema from '../../../models/Tema';
+import useLocalStorage from 'react-use-localstorage';
+import { busca } from '../../../services/service';
 
-function ListaTemas() {
-  const [temas, setTemas] = useState<Tema[]>([]);
-  const [token, setToken] = useLocalStorage("token");
-  const history = useNavigate();
+function ListaTema() {
 
-  async function getAllTemas() {
-    await getAll("/temas", setTemas, {
+  const [temas, setTemas] = useState<Tema[]>([])
+  const [token, setToken] = useLocalStorage('token')
+  const history = useNavigate()
+
+  useEffect(() => {
+    if(token === '') {
+      alert('Você precisa estar logado.')
+      history('/login')
+    }
+  }, [token])
+
+  async function getTema() {
+    await busca('/temas', setTemas, {
       headers: {
-        Authorization: token,
-      },
-    });
+        Authorization: token
+      }
+    })
   }
 
   useEffect(() => {
-    getAllTemas();
-  }, []);
-
-  useEffect(() => {
-    if (token === "") {
-      alert("Sem token não né meu bom");
-      history("/login");
-    }
-  }, []);
+    getTema()
+  }, [temas.length])
 
   return (
     <>
-      {temas.map((tema) => (
-        <Box m={4}>
-          <Card>
-            <CardContent>
-              <Typography color="textSecondary" gutterBottom>
-                Tema:
-              </Typography>
-              <Typography variant="h5" component="h2">
-                {tema.descricao}
-              </Typography>
-            </CardContent>
-            <CardActions>
-              <Button color="primary" variant="contained" size="small">
-                Editar
-              </Button>
-              <Button color="secondary" variant="contained" size="small">
-                Deletar
-              </Button>
-            </CardActions>
-          </Card>
-        </Box>
-      ))}
+    {temas.map((tema) => (
+      <Box m={2} >
+        <Card variant="outlined">
+          <CardContent>
+            <Typography color="textSecondary" gutterBottom>
+              Tema
+            </Typography>
+            <Typography variant="h5" component="h2">
+              {tema.descricao}
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Box display="flex" justifyContent="center" mb={1.5} >
+
+              <Link to={`/formularioTema/${tema.id}`} className="text-decorator-none">
+                <Box mx={1}>
+                  <Button variant="contained" className="marginLeft" size='small' color="primary" >
+                    atualizar
+                  </Button>
+                </Box>
+              </Link>
+              <Link to={`/deletarTema/${tema.id}`} className="text-decorator-none">
+                <Box mx={1}>
+                  <Button variant="contained" size='small' color="secondary">
+                    deletar
+                  </Button>
+                </Box>
+              </Link>
+            </Box>
+          </CardActions>
+        </Card>
+      </Box>
+))}
     </>
   );
 }
 
-export default ListaTemas;
+
+export default ListaTema;
